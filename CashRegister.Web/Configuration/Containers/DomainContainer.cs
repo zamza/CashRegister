436b6ce117082dab4ccba12d3c087a207eed30e0
Patrick Galagan <pmgalagan@gmail.com>
2022-01-22 12:20:14 -0500

    Added Dependency Injection to Program.cs

---
+++ CashRegister.Web/Configuration/Containers/DomainContainer.cs
@@ -0,0 +1,42 @@
+﻿using CashRegister.Domain;
+using CashRegister.Domain.Abstract;
+using CashRegister.Domain.Models;
+
+namespace CashRegister.Web.Configuration.Containers
+{
+    internal static class DomainContainer
+    {
+        internal static void ConfigureDependencies(IServiceCollection services)
+        {
+            var amounts = new Dictionary<Currencies, int>()
+            {
+                {Currencies.Penny, 2000},
+                {Currencies.Nickel, 0},
+                {Currencies.Dime, 0},
+                {Currencies.Quarter, 0},
+                {Currencies.HalfDollar, 0},
+                {Currencies.Dollar, 0},
+                {Currencies.TwoDollars, 0},
+                {Currencies.FiveDollars, 0},
+                {Currencies.TenDollars, 0},
+                {Currencies.TwentyDollars, 2}
+            };
+            var denominations = new Dictionary<Currencies, decimal>()
+            {
+                {Currencies.Penny, .01M},
+                {Currencies.Nickel, .05M},
+                {Currencies.Dime, .1M},
+                {Currencies.Quarter, .25M},
+                {Currencies.HalfDollar, .5M},
+                {Currencies.Dollar, 1M},
+                {Currencies.TwoDollars, 2M},
+                {Currencies.FiveDollars, 5M},
+                {Currencies.TenDollars, 10M},
+                {Currencies.TwentyDollars, 20M}
+            };
+            var cashRegister = new Domain.Models.CashRegister(amounts, denominations);
+
+            services.AddScoped<ICashRegister, Domain.Models.CashRegister>(x => cashRegister);
+        }
+    }
+}
---
+++ CashRegister.Web/Controllers/TransactionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CashRegister.Domain.Abstract;
+
+namespace CashRegister.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IServiceProvider _container;
+
+        public TransactionController(IServiceProvider provider)
+        {
+            _container = provider;
+        }
+
+            [HttpGet("amounts")]
+        public ActionResult GetAmounts()
+        {
+            var register = _container.GetService<ICashRegister>();
+            return Ok();
+        }
+    }
+}
